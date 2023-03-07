4961972c8468367980d719ecb0c0c5031ebe51f9
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-07 17:14:08 -0600

    Reading a json file

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+import db
 
 app = FastAPI()
+app.title = "My very first FastAPI application"
 
-@app.get("/")
+@app.get("/", tags=["Home"])
 def root():
-	return "Hello World"
+	return HTMLResponse("<h1>Hello World</h1>")
+
+@app.get("/movies", tags=["Movies"])
+def get_movies():
+	return db.data
