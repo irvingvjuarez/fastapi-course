f92a52d79c77e70fdb5506c692183f41ff81569e
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-07 17:45:33 -0600

    Using params in the endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse
 import db
 
@@ -12,3 +12,23 @@ def root():
 @app.get("/movies", tags=["Movies"])
 def get_movies():
 	return db.data
+
+@app.get("/movie/{movie_id}", tags=["Movies"])
+def get_movie_detail(movie_id: int):
+	matching_movies = list(
+		filter(lambda movie: movie["id"] == movie_id, db.data.get("data"))
+	)
+
+	matches = len(matching_movies)
+	if matches == 1:
+		return matching_movies[0]
+	elif matches > 1:
+		raise HTTPException(
+			status_code=status.HTTP_409_CONFLICT,
+			detail="There has been an error retrieving the data, please try again later"
+		)
+	else:
+		raise HTTPException(
+			status_code=status.HTTP_404_NOT_FOUND,
+			detail=f"Movie with id {movie_id} not found."
+		)
