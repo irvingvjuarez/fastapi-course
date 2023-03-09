8504d914c28e4dc42204152bce48779b47c2bf7c
Irving Juárez <irvingjuarez274@gmail.com>
2023-03-08 18:26:43 -0600

    Accessing data util done

--- db/__init__.py
+++
@@ -1,6 +0,0 @@
-import json
-
-file = open("db/data.json")
-data = json.load(file)
-
-file.close()
--- utils/get_data.py
+++ utils/get_data.py
@@ -1,7 +1,10 @@
+import json
 import sys
 sys.path.append("../db")
 
-import db
-
 def get_data():
-	return db.data.get("data")
+	file = open("db/data.json")
+	data = json.load(file)
+	file.close()
+
+	return data.get("data")
